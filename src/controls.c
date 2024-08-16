f841f4162ca9a73dfa9c777a39d170886779c3ca
Tom <tom.k.ecker@gmx.de>
2024-08-16 20:49:10 +0200

    added movement

--- src/controls.c
+++ src/controls.c
@@ -6,7 +6,7 @@
 /*   By: tomecker <tomecker@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/16 01:02:11 by dolifero          #+#    #+#             */
-/*   Updated: 2024/08/16 20:32:31 by tomecker         ###   ########.fr       */
+/*   Updated: 2024/08/16 20:45:59 by tomecker         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,30 @@
 
 void clear_image(mlx_image_t *image)
 {
-	uint32_t transparent_color = 0x00000000;
-    for (uint32_t y = 0; y < image->height; y++)
+	uint32_t transparent_color;
+	uint32_t y;
+	uint32_t x;
+
+	y = 0;
+	transparent_color = 0x00000000;
+    while(y < image->height)
     {
-        for (uint32_t x = 0; x < image->width; x++)
+		x = 0;
+        while (x < image->width)
         {
             mlx_put_pixel(image, x, y, transparent_color);
-        }
+        	x++;
+		}
+		y++;
     }
+}
+
+void	redraw(t_data *data)
+{
+		setPlane(data);
+		castRays(data);
+		clear_image(data->cubed->walls);
+		draw_walls(data->cubed, data);
 }
 
 
@@ -45,21 +61,39 @@ void	ft_window_hook(void *param)
 	{
 		data->dirX = cos(angle) * oldDirX - sin(angle) * oldDirY;
 		data->dirY = sin(angle) * oldDirX + cos(angle) * oldDirY;
-		setPlane(data);
-		castRays(data);
-		clear_image(data->cubed->walls);
-		draw_walls(data->cubed, data);
+		redraw(data);
 	}
 	if (mlx_is_key_down(mlx, MLX_KEY_LEFT))
 	{
 		data->dirX = cos(angle) * oldDirX + sin(angle) * oldDirY;
 		data->dirY = -sin(angle) * oldDirX + cos(angle) * oldDirY;
-		
-		setPlane(data);
-		castRays(data);
-		clear_image(data->cubed->walls);
-		draw_walls(data->cubed, data);
+		redraw(data);
 	}
+	if (mlx_is_key_down(mlx, MLX_KEY_W))
+	{
+		data->posX += data->dirX / 20;
+		data->posY += data->dirY / 20;
+		redraw(data);
+	}
+	if (mlx_is_key_down(mlx, MLX_KEY_S))
+	{
+		data->posX -= data->dirX / 20;
+		data->posY -= data->dirY / 20;
+		redraw(data);
+	}
+	if (mlx_is_key_down(mlx, MLX_KEY_D))
+	{
+		data->posX += data->planeX / 20;
+		data->posY += data->planeY / 20;
+		redraw(data);
+	}
+	if (mlx_is_key_down(mlx, MLX_KEY_A))
+	{
+		data->posX -= data->planeX / 20;
+		data->posY -= data->planeY / 20;
+		redraw(data);
+	}
+	
 
 }
 
