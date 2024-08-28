5d0b64dd7742c81a3216d8114f4b31d609c81634
Tom <tom.k.ecker@gmx.de>
2024-08-28 18:25:07 +0200

    added proper corner collision

--- src/controls/control_utils.c
+++ src/controls/control_utils.c
@@ -3,43 +3,55 @@
 /*                                                        :::      ::::::::   */
 /*   control_utils.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dolifero <dolifero@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tomecker <tomecker@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/17 01:25:16 by dolifero          #+#    #+#             */
-/*   Updated: 2024/08/28 14:36:23 by dolifero         ###   ########.fr       */
+/*   Updated: 2024/08/28 18:23:03 by tomecker         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/cubed.h"
 
-void	collision(t_data *data, double newX, double newY)
+int	detect(char c)
 {
-	float	buffer;
-	int		canmoveX;
-	int		canmoveY;
+	return (c == '1' || c == 'D' || c == 'F');
+}
 
-	canmoveX = 1;
-	canmoveY = 1;
+void collision(t_data *data, double newX, double newY)
+{
+    float buffer;
+    int canmoveX;
+    int canmoveY;
+	
+	canmoveY= 1;
+	canmoveX= 1;
 	buffer = 0.3;
-	if (data->Map[(int)newY][(int)newX] == 'M')
+    if (data->Map[(int)newY][(int)newX] == 'M')
 	{
-		clear_image(data->cubed->hand);
-		draw_overlay_part(data->cubed->victory, data->texture->death, 0, 0);
-	}
-  if ((ft_strchr("1DF", data->Map[(int)data->posY][(int)(newX + buffer)]) && newX > data->posX) ||
-        (ft_strchr("1DF", data->Map[(int)data->posY][(int)(newX - buffer)]) && newX < data->posX))
-     canmoveX = 0;
-  if ((ft_strchr("1DF", data->Map[(int)(newY + buffer)][(int)data->posX]) && newY > data->posY) ||
-        (ft_strchr("1DF", data->Map[(int)(newY - buffer)][(int)data->posX]) && newY < data->posY))
-     canmoveY = 0;
-	if (canmoveX && canmoveY)
+        clear_image(data->cubed->hand);
+        draw_overlay_part(data->cubed->victory, data->texture->death, 0, 0);
+    }
+    if (detect(data->Map[(int)(data->posY + buffer)][(int)(newX + buffer)]) ||
+        detect(data->Map[(int)(data->posY - buffer)][(int)(newX + buffer)]) ||
+        detect(data->Map[(int)(data->posY + buffer)][(int)(newX - buffer)]) ||
+        detect(data->Map[(int)(data->posY - buffer)][(int)(newX - buffer)]) ||
+        detect(data->Map[(int)data->posY][(int)(newX + buffer)]) ||
+        detect(data->Map[(int)data->posY][(int)(newX - buffer)]))
+        canmoveX = 0;
+    if (detect(data->Map[(int)(newY + buffer)][(int)(data->posX + buffer)]) ||
+        detect(data->Map[(int)(newY + buffer)][(int)(data->posX - buffer)]) ||
+        detect(data->Map[(int)(newY - buffer)][(int)(data->posX + buffer)]) ||
+        detect(data->Map[(int)(newY - buffer)][(int)(data->posX - buffer)]) ||
+        detect(data->Map[(int)(newY + buffer)][(int)data->posX]) ||
+        detect(data->Map[(int)(newY - buffer)][(int)data->posX]))
+        canmoveY = 0;
+    if (canmoveX && canmoveY)
 	{
-		data->posX = newX;
-		data->posY = newY;
-	}
+        data->posX = newX;
+        data->posY = newY;
+    }
 	else if (canmoveX)
-		data->posX = newX;
-	else if (canmoveY)
-		data->posY = newY;
-
+        data->posX = newX;
+    else if (canmoveY)
+        data->posY = newY;
 }
